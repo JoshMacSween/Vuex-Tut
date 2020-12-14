70f6899504d0572795a751d2da428dcca1a53429
Joshua MacSween <joshmacsween@gmail.com>
2020-12-13 18:36:04 -0700

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <h3>{{ joke }}</h3>
+    <button @click="addJoke">Add Joke</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {mapActions} from 'vuex'
+
+export default {
+  name: "Home",
+  data: () => ({
+    // joke: "",
+  }),
+  methods: {
+    ...mapActions(["setCurrentJoke"]),
+    addJoke() {
+      // this.$store.dispatch("setCurrentJoke");
+      this.setCurrentJoke()
+    },
+  },
+  computed: {
+    joke() {
+      return this.$store.getters.getCurrentJoke;
+    },
+  },
+  // mounted() {
+  //   this.joke = this.$store.getters.getCurrentJoke;
+  // },
+};
+</script>
---
+++ src/views/Jokes.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="jokes">
+    <h1>All Jokes Here</h1>
+    <h4 v-for="(joke, idx) in jokes" :key="idx">
+      <span>{{ idx + 1 }}</span>
+      {{ joke }}
+    </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    jokes() {
+      return this.$store.getters.getAllJokes;
+    },
+  },
+};
+</script>
